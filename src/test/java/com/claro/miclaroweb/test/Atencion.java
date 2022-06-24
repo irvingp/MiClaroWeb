8036bfeb4d004f596b28ce2c1f4382d6bf84dab0
shewee39 <shewee.martinez@gmail.com>
2022-06-24 16:02:05 -0600

    Se agrego la automatizacion de todos los servicios despues del login y arreglo de clase

--- src/test/java/com/claro/miclaroweb/test/Atencion.java
+++ src/test/java/com/claro/miclaroweb/test/Atencion.java
@@ -18,27 +18,27 @@ public class Atencion extends BaseClass{
 	public void AtencionTestProvider(String t5XpathAtencion ,String t5XpathBot,String t5XpathWa,String t5XpathContratacion,String t5XpathCerrarSesion) throws InterruptedException
 	{
 		time.sleep(5);
-		test.log(LogStatus.INFO, "Verificar enlace as");
+		test.log(LogStatus.INFO, "Verificar enlace a atencion");
 		WaitToClikByXpath(t5XpathAtencion, 20);		  
 		driver.findElement(By.xpath(t5XpathAtencion)).click();
 		TakeScreenShot("Atencion_1");	
 		
-		test.log(LogStatus.INFO, "Verificar enlace as");
+		test.log(LogStatus.INFO, "Verificar enlace a el chat del bot");
 		WaitToClikByXpath(t5XpathBot, 20);		  
 		driver.findElement(By.xpath(t5XpathBot)).click();
 		SwitchToOriginalWindows(); 
 		
-		test.log(LogStatus.INFO, "Verificar enlace as");
+		test.log(LogStatus.INFO, "Verificar enlace a el chat de Whatsapp");
 		WaitToClikByXpath(t5XpathWa, 20);		  
 		driver.findElement(By.xpath(t5XpathWa)).click();
 		SwitchToOriginalWindows();
 		
-		test.log(LogStatus.INFO, "Verificar enlace as");
+		test.log(LogStatus.INFO, "Verificar enlace a Contratacion y Servicios");
 		WaitToClikByXpath(t5XpathContratacion, 20);		  
 		driver.findElement(By.xpath(t5XpathContratacion)).click();
 		SwitchToOriginalWindows(); 	
 		
-		test.log(LogStatus.INFO, "Verificar enlace as");
+		test.log(LogStatus.INFO, "Verificar enlace a Cerrar sesion");
 		WaitToClikByXpath(t5XpathCerrarSesion, 20);		  
 		driver.findElement(By.xpath(t5XpathCerrarSesion)).click();
 		time.sleep(5);
